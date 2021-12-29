344d49ce8939370b5c87e7a3a96f6f31f6aca003
Anderson Vallejo <vallejoanderson@gmail.com>
2021-12-29 07:55:11 -0500

    Test commit

--- reactjs/src/components/BookItem.js
+++ reactjs/src/components/BookItem.js
@@ -5,7 +5,7 @@ export const BookItem = ( { img } ) => {
     
     return(
      
-    <div className="book">  
+    <div className="book">
 
         <div className="book__cover">
             <img className="book__image" src={ img } alt="book cover" />
